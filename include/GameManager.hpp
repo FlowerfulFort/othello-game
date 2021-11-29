9915a963fed5e2e3a4096e960611717886b618df
blueMydNight <skagyghk@naver.com>
2021-11-29 16:19:28 +0900

    Added more Information at include/.hpp about TODO

--- include/GameManager.hpp
+++ include/GameManager.hpp
@@ -1,5 +1,18 @@
+/* 추가해야 할 것.
+   보드판에 말을 놓는것은 Player들의 몫이니,
+   GameManager는 보드판을 검사하여
+   게임이 끝났는지 검사해야 합니다.
+   게임이 끝나는 조건은 크게 두가지인데,
+   1. 보드판이 꽉차거나,
+   2. 현재 턴의 플레이어가 말을 놓을 수 없는 상황일때
+   를 매 턴마다 검사해야 합니다.
+
+   1의 경우에는 턴수를 검사하여 간단히 조사할 수 있지만,
+   2의 경우가 어렵겠네요.
+*/
 #ifndef __GAMEMANAGER_HPP__
 #define __GAMEMANAGER_HPP__
+#include "Player.hpp"
 class GameManager {
  public:
     /* this class is Singleton Class. */
@@ -7,11 +20,12 @@ class GameManager {
 
     /* before "Program" start, this function must be called. */
     void GameInitialize() const;
-    
+
     /* before "Game" start, this function must be called. */
     /* It sets UI for players. */
     void WindowInitialize();
 
+    void registerPlayer(Player* p1, Player* p2);
     /* draw Player UserInterface. */
     void drawPlayerUI();
 
@@ -26,6 +40,8 @@ class GameManager {
     void ExitGame(int mode=0);
  private:
     GameManager();  // private constructor.
+    Player* p1_;
+    Player* p2_;
     void resetTerm();
     static GameManager* instance_;
     int** board_;   // othello game board.
--- include/Player.hpp
+++ include/Player.hpp
@@ -1,3 +1,19 @@
+/* 있어야 할것.
+   playercode_는 GameManagerm의 보드포인터에서 플레이어의 말을
+   구분하는데 사용한다.
+
+   플레이어가 판에 말을 주는 행위가 있어야 한다.
+   매개변수로 int** board, 좌표 y, x를 받고
+   해당 board에 playercode를 표시하는 방식으로 말을 놓는다.
+   물론 그 곳에 말을 놓을수 있는지 판단해야 하므로
+   리턴값으로 false/true로 놓을수 있는지 없는지 리턴한다.
+   놓는데 성공했다면 true를 리턴하기로 한다.
+
+   말을 놓았으면 말을 뒤집어야 한다.
+   뒤집는 코드도 역시 Player의 함수로 표현한다.
+   말을 놓음과 동시에 해도 상관없다.
+*/
+
 #ifndef __PLAYER_HPP__
 #define __PLAYER_HPP__
 #include <ncurses.h>
